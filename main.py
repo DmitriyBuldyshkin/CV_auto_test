3030e9dbb1b6e05bb8687e15b07aa287b5233a47
DmitriyBuldyshkin <dimas2528@gmail.com>
2025-04-27 14:15:07 +0500

    its working

--- main.py
+++ main.py
@@ -6,8 +6,19 @@ import time
 
 address = "https://job.mts.ru/programs/start/521648931474506519"
 button1 = "Хочу на стажировку в МТС"
-data = {
-    
+button2 = "Подать заявку"
+
+personal_data = {
+    "Имя": "Дмитрий",
+    "Фамилия": "Булдышкин",
+    "Телефон": 9173765880,
+    "Почта": "BuldyshkinDmitriy@yandex.ru",
+    "Город": "Москва"
+}
+
+education = {
+    "ВУЗ": "Уфимский университет науки и технологий",
+    "Специальность": "Автоматизация технологических процессов и производств"
 }
 
 options = webdriver.ChromeOptions()
@@ -15,19 +26,26 @@ options.add_experimental_option("prefs", {
     "profile.default_content_setting_values.geolocation": 2 
 })
 driver = webdriver.Chrome(options=options)
-
-driver.get(address)
 wait = WebDriverWait(driver, 15)
 
-elem = wait.until(EC.element_to_be_clickable((By.XPATH, f"//button[contains(., '{button1}')]")))
-driver.execute_script("arguments[0].scrollIntoView(true);", elem)
-elem.click()
+driver.get(address)
 
-div_label = wait.until(EC.presence_of_element_located((By.XPATH, "//label[text()='Имя']")))
-driver.execute_script("arguments[0].scrollIntoView(true);", div_label)
-div = div_label.find_element(By.XPATH, '..')
-name = div.find_element(By.XPATH, '//input')
-name.send_keys("Дмитрий")
+def button_click(button):
+    elem = wait.until(EC.element_to_be_clickable((By.XPATH, f"//button[contains(., '{button}')]")))
+    driver.execute_script("arguments[0].scrollIntoView(true);", elem)
+    elem.click()
+
+def data_insert(key, value):
+    div_label = wait.until(EC.presence_of_element_located((By.XPATH, f"//label[text()='{key}']")))
+    driver.execute_script("arguments[0].scrollIntoView(true);", div_label)
+    div = div_label.find_element(By.XPATH, '../..')
+    name = div.find_element(By.XPATH, './/input')
+    name.send_keys(value)
+
+button_click(button1)
+
+for key, value in personal_data.items():
+    data_insert(key, value)
 
 time.sleep(15)
 driver.quit()
